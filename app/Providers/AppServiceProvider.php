e4d378bd2f577702d9901044f675ed6a9ae05bec
cahyo14-code <cahyo081406@gmail.com>
2026-07-28 01:41:26 +0700

    Fix AppServiceProvider URL facade import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,8 +15,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (config('app.env') === 'production' || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    \URL::forceScheme('https');
-}
+        Paginator::useBootstrapFive();
+        URL::forceScheme('https');
     }
 }
